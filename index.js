ec5302f58318e9ce1d007b2ddb683aefb198f5d0
Aarav Shrivastava <aaravshrivastava4129@gmail.com>
2026-09-01 19:00:00 +0530

    Completed

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ app.command("/nobita-help", async ({ ack, respond }) => {
 `Available Commands:
 /nobita-ping - Check bot latency
 /nobita-catfact - Get a cat fact
-/nobita-joke - Tells a joke`
+/nobita-joke - Tells a joke
+/nobita-fact - Tells a fact
+/nobita-coinflip - Flips a coin
+/nobita-rolldice - Rolls a die`
   });
 });
 
@@ -35,6 +38,69 @@ app.command("/nobita-catfact", async ({ ack, respond }) => {
     await respond({ text: `Cat Fact:\n${response.data.fact}` });
   } catch (err) {
     await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/nobita-fact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://uselessfacts.jsph.pl/random.json?language=en");
+    await respond({ text: `Fact:\n${response.data.text}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a fact." });
+  }
+});
+
+app.command('/nobita-coinflip', async ({ command, ack, respond }) => {
+  await ack();
+
+  try {
+    const outcomes = [
+      { side: 'Heads'},
+      { side: 'Tails'}
+    ];
+    const result = outcomes[Math.floor(Math.random() * outcomes.length)];
+
+    await respond({
+      response_type: 'in_channel',
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `I flipped a coin... \n\n*Result:* *${result.side}*!`
+          }
+        }
+      ]
+    });
+  } catch (error) {
+    console.error('Error executing coinflip:', error);
+    await respond({ text: '❌ Failed to flip the coin. Try again!' });
+  }
+});
+
+app.command('/nobita-rolldice', async ({ command, ack, respond }) => {
+  await ack();
+
+  try {
+    const roll = Math.floor(Math.random() * 6) + 1;
+
+    await respond({
+      response_type: 'in_channel',
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `🎲I rolled a die... \n\n*Result:* *${roll}*`
+          }
+        }
+      ]
+    });
+  } catch (error) {
+    console.error('Error executing dice roll:', error);
+    await respond({ text: '❌ Failed to roll the die. Try again!' });
   }
 });
 
